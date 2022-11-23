bd40231cbbc3f4fcb21339fdc313fd2e57d63f5c
nabiulpranto <nabiulislampranto19@gmail.com>
2022-11-24 00:27:42 +0600

    pages

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className="hero my-20  bg-indigo-100	">
             <div className="hero-content text-center">
                 <div className="max-w-md">
-                    <p className="text-2xl font-bold text-indigo-900	">About Us</p>
+                    <p className="text-1xl font-bold text-indigo-900">About Us</p>
                     <h1 className="text-5xl font-bold">Hello All!</h1>
                     <p className="py-6 ">We Are Providing The Best Service in Town. If You Wanna Drink Contact with Us. <br/> Juice Bar,<br/>Banasree,Dhaka </p>
                 </div>
